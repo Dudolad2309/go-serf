64e21bb9cf9064436b1f79408d94cb6fd387ab5a
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-25 11:16:19 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows___left" src=" img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows___right"src=" img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dotshead',
+    });
+
+    $('.slider-dotshead').slick({
+
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+
+    });
+
+
+    $('.serf__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows___left" src=" img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows___right"src=" img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-map',
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.serf__slider',
+        focusOnSelect: true
+
+    })
+
+});
